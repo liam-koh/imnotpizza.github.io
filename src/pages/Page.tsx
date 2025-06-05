fb0da28d74d2163346b0b012144922cf1d8d898e
liam-koh <liam@o2pluss.com>
2025-06-05 20:57:32 +0900

    Feat: 애니메이션 관련 추가

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -5,7 +5,6 @@ import Header from '@/components/Header';
 import Hero from '@/components/Hero';
 import Projects from '@/components/Projects';
 import Skills from '@/components/Skills';
-import React from 'react';
 
 export default function Page() {
   return (
