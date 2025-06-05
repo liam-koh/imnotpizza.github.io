effe5c8b47873aaa37dd54f5ea8df6284de935de
liam-koh <liam@o2pluss.com>
2025-06-05 20:35:14 +0900

    Feat: initial settings

--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
 
 export default function Page() {
-  const [count, setCount] = React.useState(0);
-
-  const onClickCountUp = () => {
-    setCount(count + 1);
-  };
-  const onClickCountDown = () => {
-    setCount(count - 1);
-  };
-
   return (
     <div>
-      <h1>o2pluss template</h1>
-      count: {count}
-      <div className="flex gap-sm">
-        <Button size="sm" onClick={onClickCountUp}>
-          count up
-        </Button>
-        <Button size="sm" onClick={onClickCountDown}>
-          count down
-        </Button>
-      </div>
+    
     </div>
   );
 }
